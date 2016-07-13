1e2f16202259a8ce9ea643f642c89f9971a6030e
spin13 <aaa@aaa>
2016-07-13 16:17:33 +0900

    a

---
+++ ABC/ABC041_A.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ABC041_A{
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        String s=sc.next();
+        System.out.println(s.charAt(sc.nextInt()-1));
+    }
+}

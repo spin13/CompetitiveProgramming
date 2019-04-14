7d9294b5eac1282535618e364994f4f270e43273
tkd <tkd>
2019-04-14 18:58:26 +0900

    'template'

---
+++ template.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <sstream>
+#include <fstream>
+#include <cstdio>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <complex>
+#include <ctime>
+#include <bitset>
+#include <cstring>
+#include <cstdlib>
+#include <iterator>
+ 
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+ 
+typedef long long int lli;
+typedef long long ll;
+using namespace std;
+ 
+int main(){
+
+}
